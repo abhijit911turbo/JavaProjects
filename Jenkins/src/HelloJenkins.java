bc47ba5e88f8b052d3f29f114f923feda9140f9e
Abhiji Thombre <abhijit.thombre@tesco.com>
2018-05-06 15:11:04 +0530

    d

--- Jenkins/src/HelloJenkins.java
+++ Jenkins/src/HelloJenkins.java
@@ -3,7 +3,7 @@ public class HelloJenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome Abhijit Thombre");
+		Systema.out.println("Welcome Abhijit Thombre");
 	}
 
 }
